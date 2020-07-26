2387ea80b7842bc70e2854c265d2531c0b70a9f5
dlscoccia <2054@holbertonschool.com>
2020-07-26 20:40:09 +0000

    updating holberton.h functions

--- holberton.h
+++ holberton.h
@@ -6,26 +6,23 @@
 #include <stdarg.h>
 /**
  * struct conversion - This struct content.
- *@sym: Firts Element
- *@f: Second Element
- *
+ *@spec: specifier char
+ *@f: pointer to function in print list
  */
 struct conversion
 {
-	char *sym;
+	char *spec;
 	int (*f)(va_list);
 };
 typedef struct conversion conver_struct;
 
-int selector(const char *format, conver_struct print_list[], va_list arg_list);
 int _printf(const char *format, ...);
+int selector(const char *format, conver_struct print_list[], va_list arg_list);
 int put_char(char);
-int print_character(va_list);
-int print_string(va_list);
-int print_percent(va_list);
-int print_integer(va_list);
-int print_number(va_list);
-int print_rot13(va_list);
-int _binary_numbers(va_list);
+int prints_character(va_list);
+int prints_string(va_list);
+int prints_percent(va_list);
+int prints_numbers(va_list);
+int prints_rot13(va_list);
 
 #endif /* _PRINT_F_ */
